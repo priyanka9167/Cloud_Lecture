320d23c84489aef99f16ed4f6660f2e20a2f0168
Priyanka Chaurasia <chaurasia.p@northeastern.edu>
2023-12-03 16:36:28 -0500

    add test case

---
+++ whatsApp/src/test/java/com/example/whatsApp/HttpRequestTest.java
@@ -0,0 +1,25 @@
+package com.example.whatsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import  static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void dataBaseHealthCheck() throws Exception{
+        assertThat(this.restTemplate.getForObject("http://localhost:"+ port + "/actuator/health", String.class)).contains("{\"status\":\"UP\"}");
+    }
+
+}
